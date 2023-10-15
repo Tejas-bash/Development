9481469cb67efecd85cd0f43e0f37bd7d80726a6
Tejas <tejastarghale@gmail.com>
2023-10-15 13:05:51 +0530

    Added New Marks Fields in Models

--- Project/APp/models.py
+++ Project/APp/models.py
@@ -4,3 +4,4 @@ class StudentModel(models.Model):
     Name = models.CharField(max_length=100)
     Age = models.IntegerField()
     Subject = models.CharField(max_length=100)
+    Marks = models.CharField(max_length=100)
