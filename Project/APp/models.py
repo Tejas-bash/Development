fddee54ed513718848d24dbf326bf74a22dd04af
Tejas <tejastarghale@gmail.com>
2023-10-15 12:53:04 +0530

    "New Model has been Created i.e Student Model"

--- Project/APp/models.py
+++ Project/APp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class StudentModel(models.Model):
+    Name = models.CharField(max_length=100)
+    Age = models.IntegerField()
+    Subject = models.CharField(max_length=100)
